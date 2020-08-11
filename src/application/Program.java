9806cb79ecdcd650907bb90b71dc24aa1df2aa71
diegolima210288 <diego.lima201402@gmail.com>
2020-08-11 00:25:49 -0300

    Incrementação das funcionalidades na classe Program

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,56 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Client;
+import entities.Order;
+
 public class Program {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws ParseException {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.println("Enter cliente data:");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Email: ");
+		String email = sc.nextLine();
+		System.out.println("Birth date (DD/MM/YYYY): ");
+		Date birthDate = sdf.parse(sc.next());
+		
+		Client client = new Client(name, email, birthDate);
+		
+		System.out.println("Enter order data:");
+		System.out.print("Status: ");
+		String status = sc.nextLine();
+		
+		System.out.print("How many items to this order? ");
+		int n = sc.nextInt();
+		
+		for (int i=1; i<=n; i++) {
+			System.out.println("Enter #"+i+" item data: ");
+			System.out.print("Product name: ");
+			String nameProduct = sc.nextLine();
+			sc.nextLine();
+			System.out.print("Product price: ");
+			double price = sc.nextDouble();
+			System.out.print("Quantity: ");
+			int quantity = sc.nextInt();
+			
+			Order order = new Order(moment, status);
+		}
+		
+		
+		
+		sc.close();
 
 	}
 
